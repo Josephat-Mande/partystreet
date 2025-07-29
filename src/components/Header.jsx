e8ed6d23af1a80e4f9e5c2a2d7b8b7c46bcbf60c
josephat-mande <jassymande19@gmail.com>
2025-07-29 16:22:42 +0300

    fix  second  vercel build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu, X, ChevronDown } from 'lucide-react';
-import { navigationItems } from '../data/navigationData';
+import { navigationItems } from '../data/NavigationData';
 
 const Header = ({ activeSection, setActiveSection, mobileMenuOpen, setMobileMenuOpen }) => {
   return (
